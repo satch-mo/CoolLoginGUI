55af3a78bc6629effa2caa2f5f4097ace03b1e32
satch-mo <satchel0293@gmail.com>
2022-08-07 12:19:53 -0500

    initial commit

---
+++ src/GUI.java
@@ -0,0 +1,65 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel success;
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);   // set rows/columns later
+
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        passLabel = new JLabel("Password");
+        passLabel.setBounds(10,50,80,25);
+        panel.add(passLabel);
+
+        passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(10,80,80,25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        success = new JLabel("");
+        success.setBounds(10,110,300,25);
+        panel.add(success);
+
+
+        frame.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passwordText.getText();
+//        System.out.println(user + ", " + password);
+
+        if(user.equals("Satchel") && password.equals("fluffydino123")){
+            success.setText("Login Successful!");
+        }
+    }
+}
